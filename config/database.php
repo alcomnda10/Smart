d7fa170167dfa41b1b957137c738de70d02e38c5
mohamed ahmed <165292877+alcomnda10@users.noreply.github.com>
2025-06-26 10:20:44 +0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -82,23 +82,23 @@ return [
             ]) : [],
         ],
 
-    'pgsql' => [
-        'driver' => 'pgsql',
-        'url' => env('DATABASE_URL'),
-        'host' => env('DB_HOST', '127.0.0.1'),
-        'port' => env('DB_PORT', '5432'),
-        'database' => env('DB_DATABASE', 'forge'),
-        'username' => env('DB_USERNAME', 'forge'),
-        'password' => env('DB_PASSWORD', ''),
-        'charset' => env('DB_CHARSET', 'utf8'),
-        'prefix' => '',
-        'prefix_indexes' => true,
-        'search_path' => 'public',
-        'sslmode' => env('DB_SSLMODE', 'require'),
-        'options' => extension_loaded('pdo_pgsql') ? array_filter([
-            PDO::PGSQL_ATTR_SSL_MODE => 'require',
-        ]) : [],
-    ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => env('DB_CHARSET', 'utf8'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'search_path' => 'public',
+            'sslmode' => env('DB_SSLMODE', 'require'),
+            'options' => extension_loaded('pdo_pgsql') ? [] : [],
+
+        ],
+
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
@@ -150,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
